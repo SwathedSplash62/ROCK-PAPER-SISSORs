411ebf36ec259b0c0311e0ade89ecb4bfa6d9afd
SwathedSplash62 <138179868+SwathedSplash62@users.noreply.github.com>
2024-02-27 13:15:00 +1300

    Add files via upload
    Done

--- RPS.py
+++ RPS.py
@@ -1,3 +1,6 @@
+import random
+
+
 def string_checker(question, valid_ans):
     error = f"Please enter a valid option from the following list: {valid_ans}"
 
@@ -43,38 +46,6 @@ def num_check(question):
             print(error)
 
 
-def yes_no_instructions(question):
-    while True:
-        response = input(question).lower()
-        # checks user response to question
-        # only accepts yes or no
-        if response == "yes" or response == "y":
-            instructions()
-            return ""
-
-        elif response == "no" or response == "n":
-            print("You chose no")
-            return ""
-
-        else:
-            print("Please answer yes or no ")
-
-
-def yes_no(question):
-    while True:
-        response = input(question).lower()
-        # checks user response to question
-        # only accepts yes or no
-        if response == "yes" or response == "y":
-            return "yes"
-
-        elif response == "no" or response == "n":
-            return "no"
-
-        else:
-            print("Please answer yes or no ")
-
-
 def statement_generator(text, decoration):
     # Make string with five characters
     ends = decoration * 2
@@ -109,19 +80,43 @@ Type <quit> to end the game at anytime.
     return ""
 
 
+# compares user / comp choice and returns all possible results
+def rps_compare(user, comp):
+    # If same then tie
+
+    if user == comp:
+        the_result = "tie"
+
+    # winning combos
+    elif user == "paper" and comp == "rock":
+        the_result = "win"
+    elif user == "scissors" and comp == "paper" or user == "rock" and comp == "scissors":
+        the_result = "win"
+
+    # if not win/tie must be loss
+    else:
+        the_result = "lose"
+
+    return the_result
+
+
 # Main routine goes here
 
 # what the game is supposed to be
 mode = "regular"
-rounds_played = 0
 
+rounds_played = 0
+rounds_tied = 0
+rounds_lost = 0
+
+yes_no_list = ['yes', 'no']
+rps_list = ['rock', 'paper', 'scissors', 'quit']
+game_history = []
 # Title
 statement_generator("Rock, Paper, Scissors", "🗿🏳️🔪")
 
-rps_list = ["rock", "paper", "scissors", "quit"]
-
 # instructions
-want_instructions = yes_no_instructions("Do you want to see the instructions?")
+want_instructions = string_checker("Do you want to see the instructions?", yes_no_list)
 
 # Ask user for number of rounds
 num_rounds = num_check("How many rounds would you like? Push <enter> for 🚂infinite mode🚂: ")
@@ -135,12 +130,13 @@ while rounds_played < num_rounds:
 
     # rounds based on mode
     if mode == "infinite":
-        rounds_heading = f"\n👏👏👏 Round {rounds_played + 1 } (Infinite Mode) 👏👏👏"
+        rounds_heading = f"\n👏👏👏 Round {rounds_played + 1} (Infinite Mode) 👏👏👏"
     else:
-        rounds_heading = f"\n🐪🐪🐪 Round {rounds_played + 1 } of {num_rounds} 🐪🐪🐪"
+        rounds_heading = f"\n🐪🐪🐪 Round {rounds_played + 1} of {num_rounds} 🐪🐪🐪"
 
     print(rounds_heading)
-    print()
+
+    comp_choice = random.choice(rps_list[:-1])
 
     # RPS
     user_choice = string_checker("Choose: ", rps_list)
@@ -149,6 +145,26 @@ while rounds_played < num_rounds:
     # alf-f4
     if user_choice == "quit":
         break
+
+    # comp move but without the leave button
+
+    the_result = rps_compare(user_choice, comp_choice)
+
+    # adjust game lost / game tied counters and add results to game history
+    if the_result == "tie":
+        rounds_tied += 1
+        feedback = "🐲🐲 It's a tie 🐲🐲"
+    elif the_result == "lose":
+        rounds_lost += 1
+        feedback = "🌄🌄 YOU lost 🌄🌄"
+    else:
+        feedback = "🦄🦄 You won :)) 🦄🦄"
+
+    round_feedback = f"{user_choice} vs {comp_choice}, {feedback}"
+    history_item = f"Round: {rounds_played} - {round_feedback}"
+
+    print(round_feedback)
+    game_history.append(history_item)
 
     rounds_played += 1
 
@@ -160,6 +176,26 @@ while rounds_played < num_rounds:
         rounds_heading = f"\n Round {rounds_played} (Infinite Mode)"
     else:
         rounds_heading = f"\n Round {rounds_played} of {num_rounds}"
+
+# stats for nerds
+
+rounds_won = rounds_played - rounds_tied - rounds_lost
+percent_won = rounds_won / rounds_played * 100
+percent_lost = rounds_lost / rounds_played * 100
+percent_tied = 100 - percent_won - percent_lost
+
+# put stats
+
+print("💶💶💶 Game Statistics 💶💶💶")
+print(f"🦄 Won: {percent_won:.2f} \t"
+      f"🌄 Lost: {percent_lost:.2f} \t"
+      f"🐲 Tied: {percent_tied:.2f} \t")
+
+# ask user for history
+see_history = string_checker("Do you want to see your game history?", yes_no_list)
+if see_history == "yes":
+    for item in game_history:
+        print(item)
 
 print("   Thank you for playing")
 statement_generator("Rock, Paper, Scissors", "🗿🏳️🔪")
